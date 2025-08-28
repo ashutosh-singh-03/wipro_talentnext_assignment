94be5aef475052155e133f21406f4431dc13adce
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-08-28 23:34:33 +0530

    changes in path

--- Numpy & Pandas/mini_project.py
+++ Numpy & Pandas/mini_project.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 # Reading the CSV File
-df = pd.read_csv('datasetExample.csv')
+df = pd.read_csv('dataset/datasetExample.csv')
 print("\nLoaded DataFrame:")
 print(df.head())
 
